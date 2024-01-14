59788be0252eb777a38d46d3551d550755264c12
JEANS <songkc5671@naver.com>
2024-01-14 18:16:01 +0900

    #1 달리기 경주
    75% 완료. 나머지 시간초과로 map 자료구조 사용해야함.

---
+++ src/Race.java
@@ -0,0 +1,28 @@
+public class Race {
+    public static void main(String[] args) {
+        String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+        String[] callings = {"kai", "kai", "mine", "mine"};
+
+        String[] result = solution(players, callings);
+        for(String s : result) {
+            System.out.print(s + " ");
+        }
+    }
+    public static String[] solution(String[] players, String[] callings) {
+        for(int i = 0; i < callings.length; i++){
+            for(int j = 0; j < players.length; j++){
+                if(callings[i].equals(players[j])){
+                    swap(players, j, j-1);
+                    break;
+                }
+            }
+        }
+        return players;
+    }
+
+    private static <T> void swap(T[] p, int j, int k){
+        T temp = p[j];
+        p[j] = p[k];
+        p[k] = temp;
+    }
+}
